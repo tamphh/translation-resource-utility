75feaf81cd14f8d15499207aaafb0c4e2818dccf
Tam Pham <tam.pham@limeade.com>
2024-11-04 20:21:02 +0700

    Refactor command codes

--- src/main/kotlin/com/potato/Main.kt
+++ src/main/kotlin/com/potato/Main.kt
@@ -3,4 +3,9 @@ package com.potato
 import com.potato.Constants.Platform.ANDROID
 import com.potato.Converter.convert
 
-fun main() = convert("src/main/resources/test.csv", ANDROID)
+fun main() =
+    convert(
+        fileName = "src/main/resources/test.csv",
+        platform = ANDROID,
+        delimiter = ','
+    )
